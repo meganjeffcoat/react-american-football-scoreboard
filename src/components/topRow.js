765afe6ee5e2de7863e78c3f9c8bfcb50f7af4f2
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-07-24 20:59:31 -0500

    naming change

--- src/components/topRow.js
+++ src/components/topRow.js
@@ -9,12 +9,12 @@ const TopRow = (props) => {
 
                 {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-                <div className="home__score">{props.valueLions}</div>
+                <div className="home__score">{props.scoreLions}</div>
             </div>
             <div className="timer">00:03</div>
             <div className="away">
                 <h2 className="away__name">Tigers</h2>
-                <div className="away__score">{props.valueTigers}</div>
+                <div className="away__score">{props.scoreTigers}</div>
             </div>
         </div>
     )
